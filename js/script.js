fd4ac84aee47827d623070605eb7906448bf752b
Fabiano Pereira <fabianorodrigues912@gmail.com>
2023-05-01 19:28:04 -0300

    Add JS script to close sidebar
    Edit script.js to add the function to close sidebar when click outside
    or when click on link

--- js/script.js
+++ js/script.js
@@ -13,5 +13,16 @@ hamburger.addEventListener ("click", () => {
 document.querySelectorAll(".nav-lnk").forEach(n => n.
 addEventListener("click", () =>{
     hamburger.classList.remove("active");
+    navbar.classList.remove("open");
 }))
+
+
+
+
+document.onclick = function(e){
+    if(!hamburger.contains(e.target) && !navbar.contains(e.target)){
+        navbar.classList.remove("open");
+        hamburger.classList.remove("active");
+    }
+}
 
